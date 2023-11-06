4cb5df8e441d319d3e006f7446290f37a31b21f3
alifthi <alifathi8008@gmail.com>
2023-11-06 11:39:37 -0600

    distributed_optimization: methods of utils class defined

--- HW1/codes/utils.py
+++ HW1/codes/utils.py
@@ -2,6 +2,12 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 class utils:
-    def __init__(self) -> None:
+    def __init__(self,function) -> None:
+        self.function=function
+    def draw_cotours(self):
+        pass
+    def draw_optim_path(self):
+        pass
+    def plot_hist(self):
         pass
     
