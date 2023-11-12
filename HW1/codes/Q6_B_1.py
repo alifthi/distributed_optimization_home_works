ba65447e6d720f85809a726618888800146211bc
alifthi <alifathi8008@gmail.com>
2023-11-12 07:10:27 -0600

    distributed_optimization: calculate_gradient method developed to canculate the gradient of loss function

--- HW1/codes/Q6_B_1.py
+++ HW1/codes/Q6_B_1.py
@@ -1,16 +1,23 @@
 import numpy as np
-n=50
-m=500
-
 
 def project_on_set(x,C,D):
     dist=C.T@x-D
     if dist>0:
         x=x-(dist)*C/(np.linalg.norm(C)**2)
     return x
+
+def calculate_gradient(x,A,B):
+    losses=np.abs(A@x+B)
+    argmax=np.argmax(losses)
+    grad=A[argmax]
+    return grad
 if __name__=='__main__':
+    n=50
+    m=500
     A=np.random.normal(0,4,[m,n])
-    B=np.random.normal(0,4,m)
+    B=np.random.normal(0,4,[m,1])
     C=np.random.normal(0,4,[n,1])
     D=np.random.normal(0,4,1)
+    x=np.ones([n,1])
+    print(calculate_gradient(x,A,B).shape)
     
