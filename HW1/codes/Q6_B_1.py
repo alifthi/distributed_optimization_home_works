64f4922e612808bf0e1b50765b9a674613fcce6e
alifthi <alifathi8008@gmail.com>
2023-11-12 07:02:41 -0600

    distributed_optimization: projecet_on_set method developed

--- HW1/codes/Q6_B_1.py
+++ HW1/codes/Q6_B_1.py
@@ -2,9 +2,15 @@ import numpy as np
 n=50
 m=500
 
-C = np.random.normal(0,4,[m,1])
-A=np.random.normal(0,4,[m,n])
-B=np.random.normal(0,4,m)
-D=np.random.normal(0,4,[m,1])
-def project_on_set(x,c,d):
-    pass
+
+def project_on_set(x,C,D):
+    dist=C.T@x-D
+    if dist>0:
+        x=x-(dist)*C/(np.linalg.norm(C)**2)
+    return x
+if __name__=='__main__':
+    A=np.random.normal(0,4,[m,n])
+    B=np.random.normal(0,4,m)
+    C=np.random.normal(0,4,[n,1])
+    D=np.random.normal(0,4,1)
+    
