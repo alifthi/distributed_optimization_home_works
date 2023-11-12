d06ee2808189e80bae8290109be8e6510be052b3
alifthi <alifathi8008@gmail.com>
2023-11-12 08:31:41 -0600

    distributed_optimization: gradient_descent method developed. It uses constant step of learning

--- HW1/codes/Q6_B_1.py
+++ HW1/codes/Q6_B_1.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from utils import utils
 def project_on_set(x,C,D):
     dist=C.T@x-D
     if dist>0:
@@ -9,8 +9,45 @@ def project_on_set(x,C,D):
 def calculate_gradient(x,A,B):
     losses=np.abs(A@x+B)
     argmax=np.argmax(losses)
-    grad=A[argmax]
-    return grad
+    isNeg=A[argmax]@x+B[argmax]<0
+    grad=A[argmax]*((-1)**int(isNeg))
+    return grad,B[argmax]
+
+def gradient_descent(A,B,C,D,n=50,learning_dist=0.001,x0=None,iteration=None):
+    if x0==None:
+        x0=np.random.normal(size=[n,1])
+    counter=0
+    current_x = x0
+    function=lambda A,x,b:np.abs(A.T@x+b)
+    learning_rate=lambda grad:learning_dist/np.linalg.norm(grad)
+    eps=0.001
+    steps=[]
+    value=[]
+    x=[]
+    while True:
+        steps.append(counter)
+        current_x=project_on_set(current_x,C,D)
+        grad,b=calculate_gradient(current_x,A,B)
+        x.append(current_x)
+        value.append(function(grad,current_x,b))
+        alpha=learning_rate(grad)
+        grad=grad[:,None]
+        next_x=current_x-alpha*grad
+        if iteration==None:
+            if counter%1000==0:
+                print(function(grad,current_x,b))
+            if np.linalg.norm(function(grad,current_x,b)-function(grad,next_x,b)) < eps:
+                return next_x,function(grad,current_x,b), {'x':np.array(x),
+                                                 'values':np.array(value),
+                                                 'iterations':steps}
+        else:
+            if counter==iteration:
+                return next_x,function(grad,current_x,b), {'x':np.array(x),
+                                                 'values':np.array(value),
+                                                 'iterations':steps}
+        counter+=1
+        current_x=next_x
+
 if __name__=='__main__':
     n=50
     m=500
@@ -18,6 +55,5 @@ if __name__=='__main__':
     B=np.random.normal(0,4,[m,1])
     C=np.random.normal(0,4,[n,1])
     D=np.random.normal(0,4,1)
-    x=np.ones([n,1])
-    print(calculate_gradient(x,A,B).shape)
-    
+    next_x,_,diction=gradient_descent(A,B,C,D)
+    print(diction)
